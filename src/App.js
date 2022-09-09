a6e1cd24d2eba288750e18be69e0dac84002a5e4
Manuel Naranjo <manuelnaranjo@outlook.com>
2022-09-09 14:27:10 -0500

    Styling components with stylesheets and inline

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import { React, Component } from 'react'
-import './App.css';
+import './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
-
   logMessage = () => {
-    console.log("Button clicked!")
+    console.log('Button clicked!')
   }
 
-  render(){
-  return (
-    <div className="App">
-      <h1>Hi I'm a react app</h1>
-      <p>This is really working!</p>
-      <button onClick={this.logMessage}>Los message</button>
-      <Person name="Lisa" age="25"/>
-      <Person name="Rose" age="26"/>
-      <Person name="Jennie" age="25">My hobbies: Singing</Person>
-      <Person name="Jisoo" age="26"/>
-    </div>
-  );
+  render() {
+
+    const style = {
+      backgroundColor: 'white',
+      fond: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    return (
+      <div className='App'>
+        <h1>Hi I'm a react app</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.logMessage}>Log message</button>
+        <Person name='Lisa' age='25' />
+        <Person name='Rose' age='26' />
+        <Person name='Jennie' age='25'>
+          My hobbies: Singing
+        </Person>
+        <Person name='Jisoo' age='26' />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,8 @@
+import './Person.css'
+
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p>I'm {props.name} and I'm {props.age} years old!</p>
             <p>{props.children}</p>
         </div>
